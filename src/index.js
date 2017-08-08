4cfd0e417516dcfead282654454ea0befc36cae5
Vu Pham <vutpham@outlook.com>
2017-08-08 01:38:14 -0700

    Create input component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
 
 const API_KEY = "AIzaSyBHi2yIT2mowtiMeW8V46s0T3BoEwke1zc";
 
 const App = () => {
-  return <div>Hi!</div>;
+  return(
+    <div>
+    <SearchBar />
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector(".container"));
