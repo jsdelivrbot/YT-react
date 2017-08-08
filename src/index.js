e38bbf7976a09f1624fa6de6836f729c894c06d6
Vu Pham <vutpham@outlook.com>
2017-08-08 01:59:47 -0700

    Add Youtube Search API

--- src/index.js
+++ src/index.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
 
 const API_KEY = "AIzaSyBHi2yIT2mowtiMeW8V46s0T3BoEwke1zc";
 
-const App = () => {
-  return(
-    <div>
-    <SearchBar />
-    </div>
-  );
-};
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: []
+    };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (data) => {
+      this.setState({videos: data});
+    });
+  }
+
+  render() {
+    return(
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
+}
+
 
 ReactDOM.render(<App />, document.querySelector(".container"));
