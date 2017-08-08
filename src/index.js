203194fb15caa7c04ee4746e77e5c57c190c469f
Vu Pham <vutpham@outlook.com>
2017-08-08 02:13:33 -0700

    Create component for video list

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
 
 const API_KEY = "AIzaSyBHi2yIT2mowtiMeW8V46s0T3BoEwke1zc";
 
@@ -23,6 +24,7 @@ class App extends React.Component {
     return(
       <div>
         <SearchBar />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
