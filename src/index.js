f87fc89148ef32fe2d4d614bda80b43ba52ed4d5
Vu Pham <vutpham@outlook.com>
2017-08-09 03:31:20 -0700

    Add debounce code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
@@ -30,9 +31,11 @@ class App extends React.Component {
   }
 
   render() {
+    const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 500);
+
     return(
       <div>
-        <SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
+        <SearchBar onSearchTermChange={videoSearch}/>
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList
           onVideoSelect={video => this.setState({selectedVideo: video})}
